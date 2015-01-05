f758462ae4bc6e1d45995704ef84719c48c4e926
Daniel Aquino <daniel@egovtn.com>
2015-01-04 18:32:08 -0600

    The unit test for adding a throw was compiling but not passing. The programmers added code to make the add one throw unit test pass.

--- src/main/java/com/refactify/Frame.java
+++ src/main/java/com/refactify/Frame.java
@@ -1,10 +1,12 @@
 package com.refactify;
 
 public class Frame {
+    private int itsScore = 0;
     public int getScore() {
-        return 0;
+        return itsScore;
     }
 
     public void add(final int pins) {
+        itsScore += pins;
     }
 }
