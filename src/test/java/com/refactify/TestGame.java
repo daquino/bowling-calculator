de8881bc5589460596ac37b5fc9e05585a4fc8d1
Daniel Aquino <daniel@egovtn.com>
2015-01-04 19:04:54 -0600

    The programmers debate on the how fragile the Frame.add function is (can add 11); however, they decide its not necessary to protect that method since its only used within the system. They talk about calculating score from strikes and spare.  They decide the Game object would hold reference to the Frames and the Frames will need to refer to previous or future frames.  This creates a cyclic dependency between the Game and Frame classes which they both dont like.  One programmer suggests to make the Frames bound as a link list and keep references to the previous and next frames.  The other programmer is unable to visualize the need and they decide to write tests to see if they can drive out that design. They stop with the Frame class and start working on the Game class.  They end up with a game that can calculate its score similar to the frame class.  Their Game class differentiates frames by using an integer array to keep track of the pins rolled and then they introduce a method to get the score for a particular frame.

---
+++ src/test/java/com/refactify/TestGame.java
@@ -0,0 +1,36 @@
+package com.refactify;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestGame {
+
+    @Test
+    public void testOneThrow() throws Exception {
+        Game g = new Game();
+        g.add(5);
+        assertEquals(5, g.score());
+    }
+
+    @Test
+    public void testTwoThrowsNoMark() throws Exception {
+        Game g = new Game();
+        g.add(5);
+        g.add(4);
+        assertEquals(9, g.score());
+    }
+
+    @Test
+    public void testFourThrowsNoMark() throws Exception {
+        Game g = new Game();
+        g.add(5);
+        g.add(4);
+        g.add(7);
+        g.add(2);
+        assertEquals(18, g.score());
+        assertEquals(9, g.scoreForFrame(1));
+        assertEquals(18, g.scoreForFrame(2));
+
+    }
+}
